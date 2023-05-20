0a570a1cef9f8f23f3754320903dce282b7691eb
Man Ioan <31920272+johnny1990@users.noreply.github.com>
2023-05-20 17:50:08 +0300

    Implement command& position Check in NinjaTrader

--- NinjaTraderExample/Form1.Designer.cs
+++ NinjaTraderExample/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,75 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.btnRunTransaction = new System.Windows.Forms.Button();
+            this.btnPosition = new System.Windows.Forms.Button();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.Location = new System.Drawing.Point(36, 91);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(728, 342);
+            this.lstResult.TabIndex = 3;
+            // 
+            // btnRunTransaction
+            // 
+            this.btnRunTransaction.Location = new System.Drawing.Point(36, 17);
+            this.btnRunTransaction.Name = "btnRunTransaction";
+            this.btnRunTransaction.Size = new System.Drawing.Size(160, 59);
+            this.btnRunTransaction.TabIndex = 2;
+            this.btnRunTransaction.Text = "Perform Transaction";
+            this.btnRunTransaction.UseVisualStyleBackColor = true;
+            this.btnRunTransaction.Click += new System.EventHandler(this.btnRunTransaction_Click);
+            // 
+            // btnPosition
+            // 
+            this.btnPosition.Location = new System.Drawing.Point(626, 56);
+            this.btnPosition.Name = "btnPosition";
+            this.btnPosition.Size = new System.Drawing.Size(104, 29);
+            this.btnPosition.TabIndex = 4;
+            this.btnPosition.Text = "Market Pos";
+            this.btnPosition.UseVisualStyleBackColor = true;
+            this.btnPosition.Click += new System.EventHandler(this.btnPosition_Click);
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(626, 17);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(100, 20);
+            this.txtPosition.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.btnPosition);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.btnRunTransaction);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstResult;
+        private System.Windows.Forms.Button btnRunTransaction;
+        private System.Windows.Forms.Button btnPosition;
+        private System.Windows.Forms.TextBox txtPosition;
     }
 }
+
--- NinjaTraderExample/Form1.cs
+++ NinjaTraderExample/Form1.cs
@@ -1,3 +1,17 @@
+﻿using NinjaTrader.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace NinjaTraderExample
 {
     public partial class Form1 : Form
@@ -5,6 +19,27 @@ namespace NinjaTraderExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRunTransaction_Click(object sender, EventArgs e)
+        {
+            var client = new Client();
+
+            var order = client.NewOrderId();
+            var result = client.Command("Place", "32724", "/ZW", "BUY", 1, "limit", 221.60, 0, "Gtc",
+                   string.Empty, order, string.Empty, string.Empty);
+
+            lstResult.Items.Add(String.Format("Result", result));
+
+            var orders = client.Orders("32724");
+            lstResult.Items.Add(String.Format("Orders ", string.IsNullOrEmpty(orders) ? "EMPTY" : orders));
+        }
+
+        private void btnPosition_Click(object sender, EventArgs e)
+        {
+            Client NTClient = new Client();
+            int myPosition = NTClient.MarketPosition(@"/ZW", @"32724");
+            txtPosition.Text = myPosition.ToString();
         }
     }
 }
